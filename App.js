5706b0ec0c3e433cf60821d9bf4640216f3010fa
afifhibatullahh <afifhibatullah59@gmail.com>
2022-02-14 12:56:40 +0700

    set react navigation

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Beranda from './src/pages/Beranda';
-import DetailBerita from './src/pages/DetailBerita';
-import Kategori from './src/pages/Kategori';
-import Video from './src/pages/Video';
-import Bandarlpg from './src/pages/Bandarlpg';
-import Lpg from './src/pages/Lpg';
-import Sport from './src/pages/Sport';
-import Politik from './src/pages/Politik';
-import Nasional from './src/pages/Nasional';
+import "react-native-gesture-handler";
+import React from "react";
+import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Route from "./src/routes/Routes";
+
 export default function App() {
   return (
-    // <Beranda/>
-    // <DetailBerita/>
-    <Video/>
-    // <Kategori/>
-    // <Bandarlpg/>
-    // <Lpg/>
-    // <Sport/>
-    // <Politik/>
-    // <Nasional/>
+    <NavigationContainer>
+      <StatusBar />
+      <Route />
+    </NavigationContainer>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import BandarLampung from './Bandarlpg';
+import Beranda from './Beranda';
+import DetailBerita from './DetailBerita';
+import Kategori from './Kategori';
+import Lpg from './Lpg';
+import Nasional from './Nasional';
+import Video from './Video';
+
+export {
+    Video, BandarLampung, Beranda, DetailBerita, Kategori, Lpg, Nasional
+}
