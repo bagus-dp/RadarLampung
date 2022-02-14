5f194dfb5edb356290be5a1f4e5a13fd0d0c43b5
afifhibatullahh <afifhibatullah59@gmail.com>
2022-02-14 12:56:28 +0700

    add icons

---
+++ assets/icons/index.js
@@ -0,0 +1,5 @@
+import icon_home from "./icon_home.png";
+import icon_kategori from "./icon_kategori.png";
+import icon_video from "./icon_video.png";
+
+export { icon_home, icon_video, icon_kategori };
