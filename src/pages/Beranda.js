ce5cdf7fa605a379f3570872c88a091cb805e4af
afifhibatullahh <afifhibatullah59@gmail.com>
2022-02-23 11:49:36 +0700

    update beranda

--- src/pages/Beranda.js
+++ src/pages/Beranda.js
@@ -36,6 +36,7 @@ import {
   searchLogo,
 } from "../../assets/images";
 import { Kategori } from ".";
+import  Icon  from "react-native-vector-icons/Ionicons";
 
 const ListKategori = [
   {
@@ -182,7 +183,8 @@ const listBerita = [
   {
     id: 16,
     image: berita25,
-    title: "Terima Kasih Pak Airlangga, Minyak Goreng di Pasar Ini Harganya Sekarang Sudah Turun",
+    title:
+      "Terima Kasih Pak Airlangga, Minyak Goreng di Pasar Ini Harganya Sekarang Sudah Turun",
     keterangan: "Radar Lampung | Rabu 6 Januari 2022 20:30 WIB",
     kategori: "Nasional",
   },
@@ -206,53 +208,64 @@ const listBerita = [
 const listHeader = [
   {
     id: 19,
-    image: 'https://radarlampung.co.id/wp-content/uploads/2022/02/IMG-20220222-WA0016-1536x1148.jpg',
+    image:
+      "https://radarlampung.co.id/wp-content/uploads/2022/02/IMG-20220222-WA0016-1536x1148.jpg",
     title: "Winarti Warning Pengusaha Nakal yang Nekat Timbun Minyak Goreng",
     keterangan: "Radar Lampung | Rabu 6 Januari 2022 20:30",
     kategori: "Berita Terbaru",
   },
   {
     id: 20,
-    image: 'https://radarlampung.co.id/wp-content/uploads/2022/02/IMG-20220215-WA0015.jpg',
+    image:
+      "https://radarlampung.co.id/wp-content/uploads/2022/02/IMG-20220215-WA0015.jpg",
     title: "Sukarma Wijaya Jabat Pj. Sekkot Bandarlampung",
     keterangan: "Radar Lampung | Rabu 6 Januari 2022 21:00",
     kategori: "Bandar Lampung",
   },
   {
     id: 21,
-    image: 'https://radarlampung.co.id/wp-content/uploads/2020/10/images-2020-10-29T200524.723.jpeg',
+    image:
+      "https://radarlampung.co.id/wp-content/uploads/2020/10/images-2020-10-29T200524.723.jpeg",
     title: "Tambah 55 Kasus Positif Covid-19 di Tanggamus",
     keterangan: "Radar Lampung | Rabu 6 Januari 2022 21:30",
     kategori: "Lampung Raya",
   },
   {
     id: 22,
-    image: 'https://radarlampung.co.id/wp-content/uploads/2022/02/IMG_20220215_213901.jpg',
+    image:
+      "https://radarlampung.co.id/wp-content/uploads/2022/02/IMG_20220215_213901.jpg",
     title: "PPKM 386 Kabupaten/Kota di Luar Jawa-Bali Diperpanjang Dua Pekan",
     keterangan: "Radar Lampung | Rabu 6 Januari 2022 21:40",
     kategori: "Nasional",
   },
   {
     id: 23,
-    image: 'https://radarlampung.co.id/wp-content/uploads/2022/02/WhatsApp-Image-2022-02-10-at-15.36.24-1536x1006.jpeg',
-    title: "Tegas ! Ini Pesan AHY ke Ketua Partai Demokrat Lampung Soal Wajib Rangkul Semua Pihak",
+    image:
+      "https://radarlampung.co.id/wp-content/uploads/2022/02/WhatsApp-Image-2022-02-10-at-15.36.24-1536x1006.jpeg",
+    title:
+      "Tegas ! Ini Pesan AHY ke Ketua Partai Demokrat Lampung Soal Wajib Rangkul Semua Pihak",
     keterangan: "Radar Lampung | Rabu 6 Januari 2022 22:00",
     kategori: "Politik",
   },
   {
     id: 24,
-    image: 'https://radarlampung.co.id/wp-content/uploads/2022/02/WhatsApp-Image-2022-02-09-at-19.09.25-e1644412013217.jpeg',
-    title: "Raih 114 Medali, Rafflesia Swimming Club Bandar Lampung Juara Umum Damraman Championship 2022",
+    image:
+      "https://radarlampung.co.id/wp-content/uploads/2022/02/WhatsApp-Image-2022-02-09-at-19.09.25-e1644412013217.jpeg",
+    title:
+      "Raih 114 Medali, Rafflesia Swimming Club Bandar Lampung Juara Umum Damraman Championship 2022",
     keterangan: "Radar Lampung | Rabu 6 Januari 2022 22:15",
     kategori: "Sport",
-  }
-
-]
+  },
+];
 
 const Beranda = ({ navigation }) => {
   const [kategori, setKategori] = useState("Berita Terbaru");
-  const [data, setData] = useState(listBerita.filter((e) => e.kategori == "Berita Terbaru"));
-  const [header, setHeader] = useState(listHeader.filter((e) => e.kategori == "Berita Terbaru"));
+  const [data, setData] = useState(
+    listBerita.filter((e) => e.kategori == "Berita Terbaru")
+  );
+  const [header, setHeader] = useState(
+    listHeader.filter((e) => e.kategori == "Berita Terbaru")
+  );
   const setKategoriFilter = (kategori) => {
     setData([...listBerita.filter((e) => e.kategori == kategori)]);
     setHeader([...listHeader.filter((e) => e.kategori == kategori)]);
@@ -267,18 +280,17 @@ const Beranda = ({ navigation }) => {
         <TouchableOpacity>
           <Image source={darkModeLogo} style={styles.headerImg} />
         </TouchableOpacity>
-        <View style={styles.searchContainer}>
-          {/* <Image source={searchLogo} style={{ width: 24, height: 24 }} />
-                    <TouchableWithoutFeedback>
-                        <Text style={{ color: 'grey', marginLeft: 8 }}>
-                            RadarLampung.id
-                        </Text>
-                    </TouchableWithoutFeedback> */}
-          <TextInput
-            // inlineImageLeft={searchLogo}
-            placeholder="RadarLampung.id"
-            keyboardType="default"
-          />
+        <View style={styles.searchContainer}> 
+        <Icon
+              name="search"
+              size={24}
+              color="#c3c3c3"
+            />
+          <TouchableWithoutFeedback onPress={() => navigation.navigate('Search')}>
+            <Text style={{ color: "#c3c3c3", marginLeft: 8 }}>
+              RadarLampung.id
+            </Text>
+          </TouchableWithoutFeedback>
         </View>
         <TouchableOpacity>
           <Image source={logo} style={styles.headerImg} />
@@ -294,39 +306,49 @@ const Beranda = ({ navigation }) => {
                   setKategoriFilter(item.name), console.log(item.name);
                 }}
               >
-                <Text style={[styles.textBody2, kategori === item.name && styles.textActive]}>{item.name}</Text>
+                <Text
+                  style={[
+                    styles.textBody2,
+                    kategori === item.name && styles.textActive,
+                  ]}
+                >
+                  {item.name}
+                </Text>
               </TouchableOpacity>
             </View>
           )}
           keyExtractor={(item) => item.id}
           horizontal={true}
+          showsHorizontalScrollIndicator={false}
           ListFooterComponent={<View style={{ marginBottom: 50 }} />}
         />
         <ScrollView showsVerticalScrollIndicator={false}>
           <View>
-            <Text style={{ fontSize: 18, fontWeight: "bold", color: '#0057A1' }}>
+            <Text
+              style={{ fontSize: 18, fontWeight: "bold", color: "#0057A1" }}
+            >
               {kategori}
             </Text>
-            {header.map((item) =>
+            {header.map((item) => (
               <View>
                 <Image source={{ uri: item.image }} style={styles.beritaa} />
-                <Text style={styles.titleText}>
-                  {item.title}
-                </Text>
-                <Text style={{ ...styles.captionText, marginTop: -15 }}>
+                <Text style={{...styles.titleText, marginBottom:4}}>{item.title}</Text>
+                <Text style={{ ...styles.captionText, marginLeft:0  }}>
                   {item.keterangan}
                 </Text>
-              </View>)
-            }
+              </View>
+            ))}
           </View>
 
           <View style={{ marginVertical: 10 }} />
           <FlatList
             data={data}
             renderItem={({ item }) => (
-              <TouchableOpacity style={{ flexDirection: "row", marginBottom: 10 }} onPress={() => {
-                navigation.navigate("DetailBerita");
-              }}
+              <TouchableOpacity
+                style={{ flexDirection: "row", marginBottom: 10 }}
+                onPress={() => {
+                  navigation.navigate("DetailBerita");
+                }}
               >
                 <Image
                   source={item.image}
@@ -345,7 +367,6 @@ const Beranda = ({ navigation }) => {
                   </Text>
                   <Text style={styles.captionText}>{item.keterangan}</Text>
                 </View>
-
               </TouchableOpacity>
             )}
             keyExtractor={(item) => item.id}
